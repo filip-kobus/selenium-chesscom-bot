9579f583c55e958dd222cb9f9e9d6ca69046a05d
Filip <fkobus.coding@gmail.com>
2025-02-23 16:55:32 +0100

    Deleted comments from imports

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys  # Required for command-line arguments and exiting the application
-from PyQt5.QtWidgets import QApplication  # The main application class
-from app_window import MainWindow  # Assuming you have a MainWindow class in `main_window.py`
+import sys
+from PyQt5.QtWidgets import QApplication
+from app_window import MainWindow
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
